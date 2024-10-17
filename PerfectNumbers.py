a9e847d996499ae2a31a2342ae60f63f5e078307
cornelius31415 <50702246+cornelius31415@users.noreply.github.com>
2024-10-17 13:34:01 +0200

    Update PerfectNumbers.py

--- PerfectNumbers.py
+++ PerfectNumbers.py
@@ -4,17 +4,30 @@
 Created on Tue May 28 11:34:16 2024
 
 @author: cornelius
-"""
+import time
 
+print("\n------------------------------------------------------------------------------------------")
+print("                                  PERFECT NUMBERS                                  ")
+print("------------------------------------------------------------------------------------------")
 """
                 PERFECT NUMBERS
                 
             A perfect number is the sum of its divisors.
             (including 1 and excluding itself)
             The first two perfect numbers are 6 and 28.
+            
+            This code contains a brute force algorithm to 
+            calculate the first few perfect numbers and 
+            a mathematical algorithm making use of Mersenne primes
+            and the theorem Euler prooved.
 
 """
 
+#------------------------------------------------------------------------------------------
+#                                   BRUTE FORCE ALGORITHM 
+#------------------------------------------------------------------------------------------
+
+start = time.time()
 
 # checks if a number is perfect
 def is_perfect(n):
@@ -31,9 +44,6 @@ def is_perfect(n):
     else:
         return False
     
- 
-
-print(is_perfect(28))
 
 
 # creates a list of the first n perfect numbers
@@ -50,5 +60,73 @@ def perfect_numbers(n):
 
     return perfects
 
-print(perfect_numbers(4))
+
+print(f"\n  The first 4 perfect numbers are: {perfect_numbers(4)}")
+
+end = time.time()
+runtime = end-start
+print(f"\n  Runtime Brute Force:              {runtime} s \n")
+
+#------------------------------------------------------------------------------------------
+#                                   MERSENNE PRIME ALGORITHM 
+#------------------------------------------------------------------------------------------
+
+start = time.time()
+
+# checks if n is a prime
+def is_prime(n):
+    
+    for i in range(2,int((n**0.5)+1)):
+        if n % i == 0:
+            return False
+    
+    return True
+
+
+# generates a certain amount of mersenne primes
+def mersenne_prim(amount):
+    
+    # Mersenne Primzahl: 2**k - 1 
+    
+    mersenne_primes = []
+    
+    k = 2
+    while len(mersenne_primes) < amount:
+        
+        mersenne = (2 ** k) -1
+        if is_prime(mersenne) == True:
+            mersenne_primes.append((mersenne,k))
+        
+        k += 1
+        
+    
+    return mersenne_primes
+
+
+# generates a certain amount of perfect numbers
+def perfekte_zahlen(amount):
+    
+    perfects = []
+    mersennes = mersenne_prim(amount)
+    
+    for element in mersennes:
+        k = element[1]
+        mersenne = element[0]
+        
+        perfect = 2**(k-1)*mersenne
+        perfects.append(perfect)
+    
+    return perfects
+    
+print("--------------------------------------------------------------------------------------------")
+print("\n  The first 8 perfect numbers are: ",perfekte_zahlen(8),"\n")
+
+end = time.time()
+runtime = end-start
+print("  Mersenne Prime Runtime:           ",runtime,"s")
+
+
+
+
+
 
